790a479cb55c9b0817b35f294c758acda4c96809
György Straub <g.straub@partner.samsung.com>
2020-09-11 16:35:39 +0100

    build-deps.sh cleanup.
    - removed fontconfig patch, which was already applied on master;
    - the only package we need to ask vcpkg to build is dali2-toolkit;
      its dependencies will get built implicitly;
    
    Signed-off-by: György Straub <g.straub@partner.samsung.com>

--- vcpkg-script/build-deps.sh
+++ vcpkg-script/build-deps.sh
@@ -3,24 +3,7 @@
 http_proxy_ip="106.1.18.35:8080"
 https_proxy_ip=$http_proxy_ip
 
-pkg_list=( winsock2
-	   pthreads
-	   curl
-	   getopt-win32
-	   libexif
-	   libjpeg-turbo
-	   libpng
-	   giflib
-	   angle
-	   cairo
-	   fontconfig
-	   freetype
-	   harfbuzz
-	   fribidi
-	   libwebp
-	   dali-windows-dependencies
-	   dali2-core
-	   dali2-adaptor
+pkg_list=( 
 	   dali2-toolkit )
 
 patch_files=( 	'../[VCPKG]_0001_Fix_proxy_access.patch'
@@ -28,7 +11,6 @@ patch_files=( 	'../[VCPKG]_0001_Fix_proxy_access.patch'
       '../[VCPKG-angle]_0001_Apply_Fix_windows_build_error_cannot_find_definition_far.patch'
       '../[VCPKG-angle]_0002_Fix_angle_header_files.patch'
       '../[VCPKG-angle]_0003_Apply_Fix_glInvalidateFramebuffer_crash.patch'
-      '../[VCPKG-fontconfig]_0001_Apply_Source_compile_fix.patch'
       '../[VCPKG-getopt]_0001_Apply_Fix_extern_c.patch'
       '../[VCPKG-libjpeg-turbo]_0001_Apply_Fix_fill_jpeg_buffer_cb.patch'
       '../[VCPKG-pthreads]_0001_Apply_Fix_define_timespec.patch'
